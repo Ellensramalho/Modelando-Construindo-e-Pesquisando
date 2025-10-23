617632505f3727a4d0ca7d170d5483562049e713
Ellensramalho <ellen_santos_bsf@hotmail.com>
2025-10-22 21:31:08 -0300

    Questão 4 respondida.

---
+++ ComandaCafe.sql
@@ -0,0 +1,18 @@
+SELECT 
+    com.cod_comanda AS codigo_comanda,
+    com.data,
+    com.mesa,
+    com.nome_cliente,
+    SUM(ic.quantidade * ca.preco_unitario) AS valor_total_comanda
+FROM 
+    Comanda com
+JOIN 
+    ItemComanda ic ON com.cod_comanda = ic.cod_comanda
+JOIN 
+    Cardapio ca ON ic.cod_cardapio = ca.cod_cardapio
+GROUP BY 
+    com.cod_comanda, com.data, com.mesa, com.nome_cliente
+HAVING 
+    COUNT(ic.cod_cardapio) > 1
+ORDER BY 
+    com.data ASC;
